ae9b7a992b1721de9917db0afa57b405c1ff3c85
APJ-9 <akhil.jose@vonnue.com>
2022-09-28 17:16:08 +0530

    arranged the code

--- learned-things/creating-new-elements.js
+++ learned-things/creating-new-elements.js
@@ -1,27 +1,10 @@
-document.body.onload = addElement()
+const addElement = () => {
+  const newDiv = document.createElement("div");
+  newDiv.style.backgroundColor = "yellow";
+  const newContent = document.createTextNode("Hello World");
+  newDiv.appendChild(newContent);
+  const currentDiv = document.getElementById("div1");
+  document.body.append(newDiv);
+};
 
-function addElement(){
- 
-    //create a new div element
-    const newDiv = document.createElement('div')
-
-    //styling the new div
-    newDiv.style.backgroundColor = 'yellow'
-
-    //and give it some content
-    const newContent = document.createTextNode('Hello World')
-
-    //add the text node to the newly created div
-    newDiv.appendChild(newContent)
-
-    //add the newly created element and its content into the DOM
-    const currentDiv = document.getElementById('div1')
-    document.body.append(newDiv)
-}
-
-document.body.onload = () => {
-    const newDiv =document.createElement('div')
-    newDiv.style.backgroundColor='red'
-    newDiv.innerText='hello world!!!'
-    document.body.append(newDiv)
-}
+document.body.onload = addElement();
