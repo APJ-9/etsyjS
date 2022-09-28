2c4b5f4785554f15a8e74b92af7679bb2f0befc5
APJ-9 <akhil.jose@vonnue.com>
2022-09-28 12:18:27 +0530

    Modified the code using prettier

--- learned-things/creating-new-elements.js
+++ learned-things/creating-new-elements.js
@@ -1,4 +1,4 @@
-document.body.onload = addElement
+document.body.onload = addElement()
 
 function addElement(){
  
@@ -16,5 +16,12 @@ function addElement(){
 
     //add the newly created element and its content into the DOM
     const currentDiv = document.getElementById('div1')
+    document.body.append(newDiv)
+}
+
+document.body.onload = () => {
+    const newDiv =document.createElement('div')
+    newDiv.style.backgroundColor='red'
+    newDiv.innerText='hello world!!!'
     document.body.append(newDiv)
 }
